4c33fcc2f21bac264234eb7546546863b43ceac0
DJprogre33 <smirnovniki119@gmail.com>
2023-08-04 11:19:54 +0300

    fixed errors

--- app/database.py
+++ app/database.py
@@ -1,10 +1,9 @@
-from typing import AsyncGenerator
-
 from sqlalchemy import NullPool
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 from app.config import settings
+
 
 # Select the database operation mode
 if settings.MODE == "TEST":
@@ -18,9 +17,6 @@ else:
 engine = create_async_engine(database_url, **database_params)
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
 
 class Base(DeclarativeBase):
     """Base class for all SQLAlchemy tables"""
