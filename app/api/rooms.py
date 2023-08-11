23d3ed8b1aec9712083d52d85b3a088c4a013f14
DJprogre33 <smirnovniki119@gmail.com>
2023-08-11 16:11:39 +0300

    refactoring Rooms endpoints

--- app/api/rooms.py
+++ app/api/rooms.py
@@ -1,193 +1,212 @@
 from datetime import date
-from typing import Annotated, Optional
+from typing import Optional
 
 from fastapi import APIRouter, Depends, UploadFile
 from fastapi_versioning import version
 
-from app.dependencies import get_current_hotel_owner
+from app.dependencies import get_current_hotel_owner, TManagerDep
 from app.exceptions import SExstraResponse
 from app.logger import logger
 from app.models.users import Users
 from app.schemas.rooms import SRoomResponse, SRooms, SRoomsResponse
 from app.services.rooms import RoomsService
 
+
 router = APIRouter(prefix="/hotels", tags=["Rooms"])
 
 
-# @router.post(
-#     "/{hotel_id}/new",
-#     response_model=SRoomResponse,
-#     responses={
-#         400: {"model": SExstraResponse},
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def create_room(
-#     hotel_id: int,
-#     room_data: SRooms,
-#     tasks_service: Annotated[RoomsService, Depends(get_rooms_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Adds a room for a specific hotel, user must be the owner of the hotel"""
-#     new_room = await tasks_service.create_room(
-#         hotel_id=hotel_id,
-#         name=room_data.name,
-#         description=room_data.description,
-#         price=room_data.price,
-#         services=room_data.services,
-#         quantity=room_data.quantity,
-#         owner_id=current_user.id
-#     )
-#     logger.info("Room succesfully created", extra={"room_id": new_room.id})
-#     return new_room
-#
-#
-# @router.put(
-#     "/{hotel_id}/{room_id}",
-#     response_model=SRoomResponse,
-#     responses={
-#         400: {"model": SExstraResponse},
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def update_room(
-#     hotel_id: int,
-#     room_id: int,
-#     room_data: SRooms,
-#     tasks_service: Annotated[RoomsService, Depends(get_rooms_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Adds a room for a specific hotel, user must be the owner of the hotel"""
-#     new_room = await tasks_service.update_room(
-#         hotel_id=hotel_id,
-#         room_id=room_id,
-#         name=room_data.name,
-#         description=room_data.description,
-#         price=room_data.price,
-#         services=room_data.services,
-#         quantity=room_data.quantity,
-#         owner_id=current_user.id
-#     )
-#     logger.info("Room succesfully created", extra={"room_id": new_room.id})
-#     return new_room
-#
-#
-# @router.delete(
-#     "/{hotel_id}/{room_id}",
-#     response_model=SRoomResponse,
-#     responses={
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def delete_room(
-#     hotel_id: int,
-#     room_id: int,
-#     tasks_service: Annotated[RoomsService, Depends(get_rooms_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Deletes a room for a specific hotel, user must be the owner of the hotel"""
-#     deleted_room = await tasks_service.delete_room(
-#         hotel_id=hotel_id, room_id=room_id, owner_id=current_user.id
-#     )
-#     logger.info("Succesfully deleted room", extra={"deleted_room_id": deleted_room.id})
-#     return deleted_room
-#
-#
-# @router.patch(
-#     "/{hotel_id}/{room_id}/image",
-#     response_model=SRoomResponse,
-#     responses={
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# async def add_room_image(
-#     hotel_id: int,
-#     room_id: int,
-#     room_image: UploadFile,
-#     tasks_service: Annotated[RoomsService, Depends(get_rooms_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Adds an image for a specific room, user must be a hotel owner"""
-#     updated_room = await tasks_service.add_room_image(
-#         hotel_id=hotel_id, room_id=room_id, room_image=room_image, owner_id=current_user.id
-#     )
-#     logger.info(
-#         "Succesfully uploaded a room image", extra={"image_path": updated_room.image_path}
-#     )
-#     return updated_room
-#
-#
-# @router.delete(
-#     "/{hotel_id}/{room_id}/image",
-#     response_model=SRoomResponse,
-#     responses={
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# async def delete_room_image(
-#     hotel_id: int,
-#     room_id: int,
-#     tasks_service: Annotated[RoomsService, Depends(get_rooms_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Deletes an image for a specific room, user must be a hotel owner"""
-#     updated_room = await tasks_service.delete_room_image(
-#         hotel_id=hotel_id, room_id=room_id, owner_id=current_user.id
-#     )
-#     logger.info("Succesfully deleted a room image", extra={"room id": updated_room.id})
-#     return updated_room
-#
-#
-# @router.get(
-#     "/{hotel_id}/rooms",
-#     response_model=Optional[list[SRoomsResponse]],
-#     responses={
-#         400: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def get_available_hotel_rooms(
-#     hotel_id: int,
-#     date_from: date,
-#     date_to: date,
-#     tasks_service: Annotated[RoomsService, Depends(get_rooms_service)]
-# ):
-#     """Gives a list of available rooms for the selected hotel, for a certain date"""
-#     rooms = await tasks_service.get_availible_hotel_rooms(
-#         hotel_id=hotel_id, date_from=date_from, date_to=date_to
-#     )
-#     logger.info("Rooms list successfully received")
-#     return rooms
-#
-#
-# @router.get(
-#     "/{room_id}",
-#     response_model=SRoomResponse,
-#     responses={
-#         400: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def get_room(
-#     room_id: int,
-#     tasks_service: Annotated[RoomsService, Depends(get_rooms_service)]
-# ):
-#     """Returns a specific room by id"""
-#     room = tasks_service.get_room(room_id=room_id)
-#     logger.info("Room successfully received", extra={"room_id": room_id})
-#     return room
+@router.post(
+    "/{hotel_id}/new",
+    response_model=SRoomResponse,
+    responses={
+        400: {"model": SExstraResponse},
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def create_room(
+    hotel_id: int,
+    room_data: SRooms,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Adds a room for a specific hotel, user must be the owner of the hotel"""
+    new_room = await RoomsService().create_room(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        name=room_data.name,
+        description=room_data.description,
+        price=room_data.price,
+        services=room_data.services,
+        quantity=room_data.quantity,
+        owner_id=current_user.id
+    )
+    logger.info("Room succesfully created", extra={"room_id": new_room.id})
+    return new_room
+
+
+@router.put(
+    "/{hotel_id}/{room_id}",
+    response_model=SRoomResponse,
+    responses={
+        400: {"model": SExstraResponse},
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def update_room(
+    hotel_id: int,
+    room_id: int,
+    room_data: SRooms,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Adds a room for a specific hotel, user must be the owner of the hotel"""
+    updated_room = await RoomsService().update_room(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        room_id=room_id,
+        name=room_data.name,
+        description=room_data.description,
+        price=room_data.price,
+        services=room_data.services,
+        quantity=room_data.quantity,
+        owner_id=current_user.id
+    )
+    logger.info("Room succesfully created", extra={"room_id": updated_room.id})
+    return updated_room
+
+
+@router.delete(
+    "/{hotel_id}/{room_id}",
+    response_model=SRoomResponse,
+    responses={
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def delete_room(
+    hotel_id: int,
+    room_id: int,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Deletes a room for a specific hotel, user must be the owner of the hotel"""
+    deleted_room = await RoomsService().delete_room(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        room_id=room_id,
+        owner_id=current_user.id
+    )
+    logger.info("Succesfully deleted room", extra={"deleted_room_id": deleted_room.id})
+    return deleted_room
+
+
+@router.patch(
+    "/{hotel_id}/{room_id}/image",
+    response_model=SRoomResponse,
+    responses={
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+async def add_room_image(
+    hotel_id: int,
+    room_id: int,
+    room_image: UploadFile,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Adds an image for a specific room, user must be a hotel owner"""
+    updated_room = await RoomsService().add_room_image(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        room_id=room_id,
+        room_image=room_image,
+        owner_id=current_user.id
+    )
+    logger.info(
+        "Succesfully uploaded a room image", extra={"image_path": updated_room.image_path}
+    )
+    return updated_room
+
+
+@router.delete(
+    "/{hotel_id}/{room_id}/image",
+    response_model=SRoomResponse,
+    responses={
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+async def delete_room_image(
+    hotel_id: int,
+    room_id: int,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Deletes an image for a specific room, user must be a hotel owner"""
+    updated_room = await RoomsService().delete_room_image(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        room_id=room_id,
+        owner_id=current_user.id
+    )
+    logger.info("Succesfully deleted a room image", extra={"room id": updated_room.id})
+    return updated_room
+
+
+@router.get(
+    "/{hotel_id}/rooms",
+    response_model=Optional[list[SRoomsResponse]],
+    responses={
+        400: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def get_available_hotel_rooms(
+    hotel_id: int,
+    date_from: date,
+    date_to: date,
+    transaction_manager: TManagerDep
+):
+    """Gives a list of available rooms for the selected hotel, for a certain date"""
+    rooms = await RoomsService().get_availible_hotel_rooms(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        date_from=date_from,
+        date_to=date_to
+    )
+    logger.info("Rooms list successfully received", extra={"total_rooms": len(rooms)})
+    return rooms
+
+
+@router.get(
+    "/{room_id}",
+    response_model=SRoomResponse,
+    responses={
+        400: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def get_room(
+    room_id: int,
+    transaction_manager: TManagerDep
+):
+    """Returns a specific room by id"""
+    room = await RoomsService().get_room(
+        transaction_manager=transaction_manager,
+        room_id=room_id
+    )
+    logger.info("Room successfully received", extra={"room_id": room.id})
+    return room
--- app/services/rooms.py
+++ app/services/rooms.py
@@ -13,138 +13,201 @@ from app.repositories.rooms import RoomsRepository
 from app.schemas.rooms import SRoomResponse
 from app.services.hotels import HotelsService
 from app.utils.base import Base
+from app.utils.transaction_manager import ITransactionManager
 
 
 class RoomsService:
     tasks_repo: RoomsRepository = RoomsRepository
 
-    @classmethod
+    @staticmethod
     async def create_room(
-            cls,
-            hotel_id: int,
-            name: str,
-            description: str,
-            price: int,
-            services: list,
-            quantity: int,
-            owner_id: int
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        name: str,
+        description: str,
+        price: int,
+        services: list,
+        quantity: int,
+        owner_id: int
     ) -> Rooms:
-        hotel = await HotelsService.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        rooms_left = await cls.tasks_repo.get_rooms_left(hotel_id=hotel.id)
-
-        if rooms_left >= quantity:
-            return await cls.tasks_repo.insert_data(
-                hotel_id=hotel.id,
-                name=name,
-                description=description,
-                price=price,
-                services=services,
-                quantity=quantity,
+        async with transaction_manager:
+            hotel = await HotelsService().check_hotel_owner(
+                transaction_manager=transaction_manager,
+                hotel_id=hotel_id,
+                owner_id=owner_id
             )
-        logger.warning(
-            "The number of rooms exceeds the total number of rooms in the hotel",
-            extra={"rooms_left": rooms_left, "required quantity": quantity},
-        )
-        raise RoomLimitExceedException
+            rooms_left = await transaction_manager.rooms.get_rooms_left(hotel_id=hotel.id)
 
-    @classmethod
+            if rooms_left >= quantity:
+                new_room = await transaction_manager.rooms.insert_data(
+                    hotel_id=hotel.id,
+                    name=name,
+                    description=description,
+                    price=price,
+                    services=services,
+                    quantity=quantity,
+                )
+                await transaction_manager.commit()
+                return new_room
+            logger.warning(
+                "The number of rooms exceeds the total number of rooms in the hotel",
+                extra={"rooms_left": rooms_left, "required quantity": quantity},
+            )
+            raise RoomLimitExceedException
+
+    @staticmethod
     async def update_room(
-            cls,
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        room_id: int,
+        name: str,
+        description: str,
+        price: int,
+        services: list,
+        quantity: int,
+        owner_id: int
+    ) -> Rooms:
+        hotel = await HotelsService().check_hotel_owner(
+            transaction_manager=transaction_manager,
+            hotel_id=hotel_id,
+            owner_id=owner_id
+        )
+        async with transaction_manager:
+            room = await transaction_manager.rooms.find_one_or_none(id=room_id)
+            if not room:
+                raise IncorrectRoomIDException
+
+            rooms_left = await transaction_manager.rooms.get_rooms_left(hotel_id=hotel.id)
+            if rooms_left + room.quantity >= quantity:
+                updated_room = await transaction_manager.rooms.update_fields_by_id(
+                    entity_id=room.id,
+                    hotel_id=hotel.id,
+                    name=name,
+                    description=description,
+                    price=price,
+                    services=services,
+                    quantity=quantity,
+                )
+                await transaction_manager.commit()
+                return updated_room
+            logger.warning(
+                "The number of rooms exceeds the total number of rooms in the hotel",
+                extra={"rooms_left": rooms_left, "required quantity": quantity},
+            )
+            raise RoomLimitExceedException
+
+    @staticmethod
+    async def delete_room(
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        room_id: int,
+        owner_id: int
+    ) -> Rooms:
+        async with transaction_manager:
+            hotel = await HotelsService().check_hotel_owner(
+                transaction_manager=transaction_manager,
+                hotel_id=hotel_id,
+                owner_id=owner_id
+            )
+            room = await transaction_manager.rooms.find_one_or_none(id=room_id, hotel_id=hotel.id)
+            if not room:
+                logger.warning(
+                    "Incorrect hotel_id or room_id",
+                    extra={"hotel_id": hotel.id, "room_id": room.id},
+                )
+                raise IncorrectRoomIDException
+            deleted_room = await transaction_manager.rooms.delete(id=room.id, hotel_id=hotel.id)
+            await transaction_manager.commit()
+            return deleted_room
+
+    @staticmethod
+    async def add_room_image(
+            transaction_manager: ITransactionManager,
             hotel_id: int,
             room_id: int,
-            name: str,
-            description: str,
-            price: int,
-            services: list,
-            quantity: int,
+            room_image: UploadFile,
             owner_id: int
     ) -> Rooms:
-        hotel = await HotelsService.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        room = await cls.tasks_repo.find_one_or_none(id=room_id)
-        if not room:
-            raise IncorrectRoomIDException
-
-        rooms_left = await cls.tasks_repo.get_rooms_left(hotel_id=hotel.id)
-        if rooms_left + room.quantity >= quantity:
-            return await cls.tasks_repo.update_fields_by_id(
-                entity_id=room.id,
-                hotel_id=hotel.id,
-                name=name,
-                description=description,
-                price=price,
-                services=services,
-                quantity=quantity,
-            )
-        logger.warning(
-            "The number of rooms exceeds the total number of rooms in the hotel",
-            extra={"rooms_left": rooms_left, "required quantity": quantity},
+        hotel = await HotelsService().check_hotel_owner(
+            transaction_manager=transaction_manager,
+            hotel_id=hotel_id,
+            owner_id=owner_id
         )
-        raise RoomLimitExceedException
+        async with transaction_manager:
+            room = await transaction_manager.rooms.find_one_or_none(id=room_id, hotel_id=hotel.id)
+            if not room:
+                logger.warning(
+                    "Incorrect hotel_id or room_id",
+                    extra={"hotel_id": hotel.id, "room_id": room.id},
+                )
+                raise IncorrectRoomIDException
 
-    @classmethod
-    async def delete_room(cls, hotel_id: int, room_id: int, owner_id: int) -> Rooms:
-        hotel = await HotelsService.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        room = await cls.tasks_repo.find_one_or_none(id=room_id, hotel_id=hotel.id)
-        if not room:
-            logger.warning(
-                "Incorrect hotel_id or room_id",
-                extra={"hotel_id": hotel.id, "room_id": room.id},
+            if room.image_path:
+                os.remove(room.image_path)
+            room_image.filename = str(uuid.uuid4())
+            file_path = f"app/static/images/rooms/{room_image.filename}.webp"
+
+            with open(file_path, "wb") as file:
+                shutil.copyfileobj(room_image.file, file)
+            updated_room = await transaction_manager.rooms.update_fields_by_id(
+                entity_id=room_id, image_path=file_path
             )
-            raise IncorrectRoomIDException
-        return await cls.tasks_repo.delete(id=room.id, hotel_id=hotel.id)
+            await transaction_manager.commit()
+            return updated_room
 
-    @classmethod
-    async def add_room_image(
-            cls, hotel_id: int, room_id: int, room_image: UploadFile, owner_id: int
+    @staticmethod
+    async def delete_room_image(
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        room_id: int,
+        owner_id: int
     ) -> Rooms:
-        hotel = await HotelsService.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        room = await cls.tasks_repo.find_one_or_none(id=room_id, hotel_id=hotel.id)
-        if not room:
-            logger.warning(
-                "Incorrect hotel_id or room_id",
-                extra={"hotel_id": hotel.id, "room_id": room.id},
+        async with transaction_manager:
+            hotel = await HotelsService().check_hotel_owner(
+                transaction_manager=transaction_manager,
+                hotel_id=hotel_id,
+                owner_id=owner_id
             )
-            raise IncorrectRoomIDException
-        if room.image_path:
-            os.remove(room.image_path)
-        room_image.filename = str(uuid.uuid4())
-        file_path = f"app/static/images/rooms/{room_image.filename}.webp"
-        with open(file_path, "wb") as file:
-            shutil.copyfileobj(room_image.file, file)
-        return await cls.tasks_repo.update_fields_by_id(
-            entity_id=room_id, image_path=file_path
-        )
-
-    @classmethod
-    async def delete_room_image(cls, hotel_id: int, room_id: int, owner_id: int) -> Rooms:
-        hotel = await HotelsService.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        room = await cls.tasks_repo.find_one_or_none(id=room_id, hotel_id=hotel.id)
-        if not room:
-            logger.warning(
-                "Incorrect hotel_id or room_id",
-                extra={"hotel_id": hotel.id, "room_id": room.id},
+            room = await transaction_manager.rooms.find_one_or_none(id=room_id, hotel_id=hotel.id)
+            if not room:
+                logger.warning(
+                    "Incorrect hotel_id or room_id",
+                    extra={"hotel_id": hotel.id, "room_id": room.id},
+                )
+                raise IncorrectRoomIDException
+            if room.image_path:
+                os.remove(room.image_path)
+            updated_room = await transaction_manager.rooms.update_fields_by_id(
+                entity_id=room_id, image_path=""
             )
-            raise IncorrectRoomIDExceptionv
-        if room.image_path:
-            os.remove(room.image_path)
-        return await cls.tasks_repo.update_fields_by_id(
-            entity_id=room_id, image_path=""
-        )
+            await transaction_manager.commit()
+            return updated_room
 
-    @classmethod
+    @staticmethod
     async def get_availible_hotel_rooms(
-        cls, hotel_id: int, date_from: date, date_to: date
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        date_from: date,
+        date_to: date
     ) -> Optional[list[SRoomResponse]]:
         date_from, date_to = Base.validate_data_range(date_from, date_to)
-        hotel = await HotelsService.get_hotel_by_id(hotel_id=hotel_id)
-        return await cls.tasks_repo.get_available_hotel_rooms(
-            hotel_id=hotel.id, date_from=date_from, date_to=date_to
-        )
+        async with transaction_manager:
+            hotel = await HotelsService().get_hotel_by_id(
+                transaction_manager=transaction_manager,
+                hotel_id=hotel_id
+            )
+            rooms = await transaction_manager.rooms.get_available_hotel_rooms(
+                hotel_id=hotel.id, date_from=date_from, date_to=date_to
+            )
+            await transaction_manager.commit()
+            return rooms
 
-    @classmethod
-    async def get_room(cls, room_id: int) -> Rooms:
-        room = await cls.tasks_repo.find_one_or_none(id=room_id)
-        if not room:
-            logger.warning("Incorrect room_id", extra={"room_id": room_id})
-            raise IncorrectRoomIDException
-        return room
+    @staticmethod
+    async def get_room(transaction_manager: ITransactionManager, room_id: int) -> Rooms:
+        async with transaction_manager:
+            room = await transaction_manager.rooms.find_one_or_none(id=room_id)
+            if not room:
+                logger.warning("Incorrect room_id", extra={"room_id": room_id})
+                raise IncorrectRoomIDException
+            await transaction_manager.commit()
+            return room
