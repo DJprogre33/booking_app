2dc63475822d631888b42e1009fe12fa04acd910
DJprogre33 <smirnovniki119@gmail.com>
2023-08-11 14:08:23 +0300

    refactoring Hotels endpoints

--- app/api/hotels.py
+++ app/api/hotels.py
@@ -1,190 +1,206 @@
 from datetime import date
-from typing import Annotated, Optional
+from typing import Optional
 
 from fastapi import APIRouter, Depends, UploadFile
 from fastapi_cache.decorator import cache
 from fastapi_versioning import version
 
-from app.dependencies import get_current_hotel_owner
+from app.dependencies import get_current_hotel_owner, TManagerDep
 from app.exceptions import SExstraResponse
 from app.logger import logger
 from app.models.users import Users
 from app.schemas.hotels import SHotel, SHotelResponse, SHotelsResponse
 from app.services.hotels import HotelsService
 
+
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
 
 
-# @router.post(
-#     "/new",
-#     response_model=SHotelResponse,
-#     responses={
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def create_hotel(
-#     new_hotel: SHotel,
-#     tasks_service: Annotated[HotelsService, Depends(get_hotels_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Creates a hotel if the user has the hotel_owner role"""
-#     new_hotel = await tasks_service.create_hotel(
-#         name=new_hotel.name,
-#         location=new_hotel.location,
-#         services=new_hotel.services,
-#         rooms_quantity=new_hotel.rooms_quantity,
-#         owner_id=current_user.id
-#     )
-#     logger.info("Succesful created a new hotel", extra={"new_hotel_id": new_hotel.id})
-#     return new_hotel
-#
-#
-# @router.put(
-#     "/{hotel_id}",
-#     response_model=SHotelResponse,
-#     responses={
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse},
-#     }
-# )
-# @version(1)
-# async def update_hotel(
-#     hotel_id: int,
-#     new_hotel: SHotel,
-#     tasks_service: Annotated[HotelsService, Depends(get_hotels_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Updates a hotel if the user has the hotel_owner role"""
-#     updated_hotel = await tasks_service.update_hotel(
-#         hotel_id=hotel_id,
-#         name=new_hotel.name,
-#         location=new_hotel.location,
-#         services=new_hotel.services,
-#         rooms_quantity=new_hotel.rooms_quantity,
-#         owner_id=current_user.id
-#     )
-#     logger.info("Succesful created a new hotel", extra={"new_hotel_id": updated_hotel.id})
-#     return updated_hotel
-#
-#
-# @router.patch(
-#     "/{hotel_id}/image",
-#     response_model=SHotelResponse,
-#     responses={
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def add_hotel_image(
-#     hotel_id: int,
-#     hotel_image: UploadFile,
-#     tasks_service: Annotated[HotelsService, Depends(get_hotels_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Adds an image for the created hotel, current user must be a hotel owner"""
-#     result = await tasks_service.add_hotel_image(
-#         hotel_id=hotel_id, hotel_image=hotel_image, owner_id=current_user.id
-#     )
-#     logger.info(
-#         "Succesfully uploaded a hotel image", extra={"image_path": result.image_path}
-#     )
-#     return result
-#
-#
-# @router.delete(
-#     "/{hotel_id}/image",
-#     response_model=SHotelResponse,
-#     responses={
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def delete_hotel_image(
-#     hotel_id: int,
-#     tasks_service: Annotated[HotelsService, Depends(get_hotels_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Deletes the image for a hotel by id, current user must be a hotel owner"""
-#     hotel = await tasks_service.delete_hotel_image(
-#         hotel_id=hotel_id, owner_id=current_user.id
-#     )
-#     logger.info(
-#         "Succesfully deleted a hotel image",
-#         extra={"hotel_with_deleted_image_id": hotel.id},
-#     )
-#     return hotel
-#
-#
-# @router.delete(
-#     "/{hotel_id}",
-#     response_model=SHotelResponse,
-#     responses={
-#         401: {"model": SExstraResponse},
-#         403: {"model": SExstraResponse},
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# async def delete_hotel(
-#     hotel_id: int,
-#     tasks_service: Annotated[HotelsService, Depends(get_hotels_service)],
-#     current_user: Users = Depends(get_current_hotel_owner)
-# ):
-#     """Deletes a hotel by id if the user is the owner of the hotel"""
-#     hotel = await tasks_service.delete_hotel(
-#         hotel_id=hotel_id, owner_id=current_user.id
-#     )
-#     logger.info(
-#         "Succesfully deleted hotel", extra={"deleted_hotel_id": hotel}
-#     )
-#     return hotel
-#
-#
-# @router.get(
-#     "/{location}",
-#     response_model=Optional[list[SHotelsResponse]],
-#     responses={
-#         400: {"model": SExstraResponse}
-#     }
-# )
-# @cache(expire=60)
-# @version(1)
-# async def get_hotels_by_location_and_time(
-#     location: str,
-#     date_from: date,
-#     date_to: date,
-#     tasks_service: Annotated[HotelsService, Depends(get_hotels_service)],
-# ):
-#     """Gives a list of hotels with free rooms for a certain date"""
-#     hotels = await tasks_service.get_hotels_by_location_and_time(
-#         location=location, date_from=date_from, date_to=date_to
-#     )
-#     logger.info("Succesful return hotels")
-#     return hotels
-#
-#
-# @router.get(
-#     "/id/{hotel_id}",
-#     response_model=SHotelResponse,
-#     responses={
-#         404: {"model": SExstraResponse}
-#     }
-# )
-# @version(1)
-# @cache(expire=60)
-# async def get_hotel_by_id(
-#     hotel_id: int, tasks_service: Annotated[HotelsService, Depends(get_hotels_service)]
-# ):
-#     """Returns the hotel by id"""
-#     hotel = await tasks_service.get_hotel_by_id(hotel_id=hotel_id)
-#     logger.info(
-#         "Succesfully return a hotel", extra={"hotel_id": hotel.id}
-#     )
-#     return hotel
+@router.post(
+    "/new",
+    response_model=SHotelResponse,
+    responses={
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def create_hotel(
+    new_hotel: SHotel,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Creates a hotel if the user has the hotel_owner role"""
+    new_hotel = await HotelsService().create_hotel(
+        transaction_manager=transaction_manager,
+        name=new_hotel.name,
+        location=new_hotel.location,
+        services=new_hotel.services,
+        rooms_quantity=new_hotel.rooms_quantity,
+        owner_id=current_user.id
+    )
+    logger.info("Succesful created a new hotel", extra={"new_hotel_id": new_hotel.id})
+    return new_hotel
+
+
+@router.put(
+    "/{hotel_id}",
+    response_model=SHotelResponse,
+    responses={
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse},
+    }
+)
+@version(1)
+async def update_hotel(
+    hotel_id: int,
+    new_hotel: SHotel,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Updates a hotel if the user has the hotel_owner role"""
+    updated_hotel = await HotelsService().update_hotel(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        name=new_hotel.name,
+        location=new_hotel.location,
+        services=new_hotel.services,
+        rooms_quantity=new_hotel.rooms_quantity,
+        owner_id=current_user.id
+    )
+    logger.info("Succesful created a new hotel", extra={"new_hotel_id": updated_hotel.id})
+    return updated_hotel
+
+
+@router.patch(
+    "/{hotel_id}/image",
+    response_model=SHotelResponse,
+    responses={
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def add_hotel_image(
+    hotel_id: int,
+    hotel_image: UploadFile,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Adds an image for the created hotel, current user must be a hotel owner"""
+    updated_hotel = await HotelsService().add_hotel_image(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        hotel_image=hotel_image,
+        owner_id=current_user.id
+    )
+    logger.info(
+        "Succesfully uploaded a hotel image", extra={"image_path": updated_hotel.image_path}
+    )
+    return updated_hotel
+
+
+@router.delete(
+    "/{hotel_id}/image",
+    response_model=SHotelResponse,
+    responses={
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def delete_hotel_image(
+    hotel_id: int,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Deletes the image for a hotel by id, current user must be a hotel owner"""
+    deleted_hotel = await HotelsService().delete_hotel_image(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        owner_id=current_user.id
+    )
+    logger.info(
+        "Succesfully deleted a hotel image",
+        extra={"hotel_with_deleted_image_id": deleted_hotel.id},
+    )
+    return deleted_hotel
+
+
+@router.delete(
+    "/{hotel_id}",
+    response_model=SHotelResponse,
+    responses={
+        401: {"model": SExstraResponse},
+        403: {"model": SExstraResponse},
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+async def delete_hotel(
+    hotel_id: int,
+    transaction_manager: TManagerDep,
+    current_user: Users = Depends(get_current_hotel_owner)
+):
+    """Deletes a hotel by id if the user is the owner of the hotel"""
+    deleted_hotel = await HotelsService().delete_hotel(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id,
+        owner_id=current_user.id
+    )
+    logger.info(
+        "Succesfully deleted hotel", extra={"deleted_hotel_id": deleted_hotel.id}
+    )
+    return deleted_hotel
+
+
+@router.get(
+    "/{location}",
+    response_model=Optional[list[SHotelsResponse]],
+    responses={
+        400: {"model": SExstraResponse}
+    }
+)
+@cache(expire=60)
+@version(1)
+async def get_hotels_by_location_and_time(
+    location: str,
+    date_from: date,
+    date_to: date,
+    transaction_manager: TManagerDep,
+):
+    """Gives a list of hotels with free rooms for a certain date"""
+    hotels = await HotelsService().get_hotels_by_location_and_time(
+        transaction_manager=transaction_manager,
+        location=location,
+        date_from=date_from,
+        date_to=date_to
+    )
+    logger.info("Succesful return hotels")
+    return hotels
+
+
+@router.get(
+    "/id/{hotel_id}",
+    response_model=SHotelResponse,
+    responses={
+        404: {"model": SExstraResponse}
+    }
+)
+@version(1)
+@cache(expire=60)
+async def get_hotel_by_id(
+    hotel_id: int, transaction_manager: TManagerDep
+):
+    """Returns the hotel by id"""
+    hotel = await HotelsService().get_hotel_by_id(
+        transaction_manager=transaction_manager,
+        hotel_id=hotel_id
+    )
+    logger.info(
+        "Succesfully return a hotel", extra={"hotel_id": hotel.id}
+    )
+    return hotel
--- app/services/hotels.py
+++ app/services/hotels.py
@@ -4,53 +4,62 @@ import uuid
 from datetime import date
 from typing import Optional
 
-from fastapi import Request, UploadFile
+from fastapi import UploadFile
 
-# from app.auth.auth import get_current_user, get_token
 from app.exceptions import AccessDeniedException, IncorrectHotelIDException
 from app.logger import logger
 from app.models.hotels import Hotels
 from app.repositories.hotels import HotelsRepository
-from app.schemas.hotels import SHotel, SHotelsResponse
+from app.schemas.hotels import SHotelsResponse
 from app.utils.base import Base
+from app.utils.transaction_manager import ITransactionManager
 
 
 class HotelsService:
     tasks_repo: HotelsRepository = HotelsRepository
 
-    @classmethod
-    async def check_hotel_owner(cls, hotel_id: int, owner_id: int) -> Hotels:
-        hotel = await cls.tasks_repo.find_one_or_none(id=hotel_id)
-        if not hotel:
-            logger.warning("Incorrect hotel id", extra={"hotel_id": hotel_id})
-            raise IncorrectHotelIDException
-        if hotel.owner_id != owner_id:
-            logger.warning(
-                "User isn't an owner", extra={"hotel_id": hotel_id, "user_id": owner_id}
-            )
-            raise AccessDeniedException
-        return hotel
+    @staticmethod
+    async def check_hotel_owner(
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        owner_id: int
+    ) -> Hotels:
+        async with transaction_manager:
+            hotel = await transaction_manager.hotels.find_one_or_none(id=hotel_id)
+            if not hotel:
+                logger.warning("Incorrect hotel id", extra={"hotel_id": hotel_id})
+                raise IncorrectHotelIDException
+            if hotel.owner_id != owner_id:
+                logger.warning(
+                    "User isn't an owner", extra={"hotel_id": hotel_id, "user_id": owner_id}
+                )
+                raise AccessDeniedException
+            await transaction_manager.commit()
+            return hotel
 
-    @classmethod
+    @staticmethod
     async def create_hotel(
-            cls,
+            transaction_manager: ITransactionManager,
             name: str,
             location: str,
             services: list,
             rooms_quantity: int,
             owner_id: int
     ) -> Hotels:
-        return await cls.tasks_repo.insert_data(
-            name=name,
-            location=location,
-            services=services,
-            rooms_quantity=rooms_quantity,
-            owner_id=owner_id,
-        )
+        async with transaction_manager:
+            new_hotel = await transaction_manager.hotels.insert_data(
+                name=name,
+                location=location,
+                services=services,
+                rooms_quantity=rooms_quantity,
+                owner_id=owner_id
+            )
+            await transaction_manager.commit()
+            return new_hotel
 
-    @classmethod
     async def update_hotel(
-            cls,
+            self,
+            transaction_manager: ITransactionManager,
             hotel_id: int,
             name: str,
             location: str,
@@ -58,54 +67,101 @@ class HotelsService:
             rooms_quantity: int,
             owner_id: int
     ) -> Hotels:
-        hotel = await cls.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        return await cls.tasks_repo.update_fields_by_id(
-            entity_id=hotel.id,
-            name=name,
-            location=location,
-            services=services,
-            rooms_quantity=rooms_quantity,
-            owner_id=owner_id,
-        )
+        async with transaction_manager:
+            current_hotel = await self.check_hotel_owner(
+                transaction_manager=transaction_manager,
+                hotel_id=hotel_id,
+                owner_id=owner_id
+            )
+            updated_hotel = await transaction_manager.hotels.update_fields_by_id(
+                entity_id=current_hotel.id,
+                name=name,
+                location=location,
+                services=services,
+                rooms_quantity=rooms_quantity,
+                owner_id=owner_id,
+            )
+            await transaction_manager.commit()
+            return updated_hotel
 
-    @classmethod
     async def add_hotel_image(
-        cls, hotel_id: int, hotel_image: UploadFile, owner_id: int
+        self,
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        hotel_image: UploadFile,
+        owner_id: int
     ) -> Hotels:
-        hotel = await cls.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        if hotel.image_path:
-            os.remove(hotel.image_path)
+        async with transaction_manager:
+            current_hotel = await self.check_hotel_owner(
+                transaction_manager=transaction_manager,
+                hotel_id=hotel_id,
+                owner_id=owner_id
+            )
+            if current_hotel.image_path:
+                os.remove(current_hotel.image_path)
 
-        hotel_image.filename = str(uuid.uuid4())
-        file_path = f"app/static/images/hotels/{hotel_image.filename}.webp"
-        with open(file_path, "wb") as file:
-            shutil.copyfileobj(hotel_image.file, file)
-        return await cls.tasks_repo.update_fields_by_id(hotel_id, image_path=file_path)
+            hotel_image.filename = str(uuid.uuid4())
+            file_path = f"app/static/images/hotels/{hotel_image.filename}.webp"
+            with open(file_path, "wb") as file:
+                shutil.copyfileobj(hotel_image.file, file)
+            updated_hotel = await transaction_manager.hotels.update_fields_by_id(hotel_id, image_path=file_path)
+            await transaction_manager.commit()
+            return updated_hotel
 
-    @classmethod
-    async def delete_hotel_image(cls, hotel_id: int, owner_id: int) -> Hotels:
-        hotel = await cls.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        if hotel.image_path:
-            os.remove(hotel.image_path)
-        return await cls.tasks_repo.update_fields_by_id(hotel_id, image_path="")
+    async def delete_hotel_image(
+        self,
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        owner_id: int
+    ) -> Hotels:
+        async with transaction_manager:
+            current_hotel = await self.check_hotel_owner(
+                transaction_manager=transaction_manager,
+                hotel_id=hotel_id,
+                owner_id=owner_id
+            )
+            if current_hotel.image_path:
+                os.remove(current_hotel.image_path)
+            updated_hotel = await transaction_manager.hotels.update_fields_by_id(hotel_id, image_path="")
+            await transaction_manager.commit()
+            return updated_hotel
 
-    @classmethod
-    async def delete_hotel(cls, hotel_id: int, owner_id: int) -> Hotels:
-        hotel = await cls.check_hotel_owner(hotel_id=hotel_id, owner_id=owner_id)
-        return await cls.tasks_repo.delete(id=hotel.id)
+    async def delete_hotel(
+        self,
+        transaction_manager: ITransactionManager,
+        hotel_id: int,
+        owner_id: int
+    ) -> Hotels:
+        async with transaction_manager:
+            current_hotel = await self.check_hotel_owner(
+                transaction_manager=transaction_manager,
+                hotel_id=hotel_id,
+                owner_id=owner_id
+            )
+            deleted_hotel = await transaction_manager.hotels.delete(id=current_hotel.id)
+            await transaction_manager.commit()
+            return deleted_hotel
 
-    @classmethod
+    @staticmethod
     async def get_hotels_by_location_and_time(
-        cls, location: str, date_from: date, date_to: date
+        transaction_manager: ITransactionManager,
+        location: str,
+        date_from: date,
+        date_to: date
     ) -> Optional[list[SHotelsResponse]]:
         date_from, date_to = Base.validate_data_range(date_from, date_to)
-        return await cls.tasks_repo.get_hotels_by_location_and_time(
-            location=location, date_from=date_from, date_to=date_to
-        )
+        async with transaction_manager:
+            hotels = await transaction_manager.hotels.get_hotels_by_location_and_time(
+                location=location, date_from=date_from, date_to=date_to
+            )
+            await transaction_manager.commit()
+            return hotels
 
-    @classmethod
-    async def get_hotel_by_id(cls, hotel_id: int) -> Hotels:
-        hotel = await cls.tasks_repo.find_one_or_none(id=hotel_id)
-        if not hotel:
-            raise IncorrectHotelIDException
-        return hotel
+    @staticmethod
+    async def get_hotel_by_id(transaction_manager: ITransactionManager, hotel_id: int) -> Hotels:
+        async with transaction_manager:
+            hotel = await transaction_manager.hotels.find_one_or_none(id=hotel_id)
+            if not hotel:
+                raise IncorrectHotelIDException
+            await transaction_manager.commit()
+            return hotel
